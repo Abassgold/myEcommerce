546057cde615cb926e36a5bf5dae2dc13fe4251f
Abassgold <Abasskola10@gmail.com>
2024-07-25 07:44:47 +0100

    added env

--- src/component/AddToCart/AddToCart.jsx
+++ src/component/AddToCart/AddToCart.jsx
@@ -16,6 +16,7 @@ import { Box, Modal, Typography } from '@mui/material';
 
 const AddToCart = () => {
     const [openModal, setOpenModal] = useState(false);
+    const [input, setInput] = useState('')
     const { filter } = useContext(searchContext)
     const [quantity, setQuantity] = useState(1)
     const [show, setshow] = useState(false)
@@ -24,7 +25,7 @@ const AddToCart = () => {
     const { keyword } = useParams()
     const [category, setCategory] = useState('');
 
-    const URI = `http://localhost:5000/admin/all-products?page=${currentPage}&filter=${filter}`
+    const URI = `${import.meta.env.VITE_URI}/admin/all-products?page=${currentPage}&filter=${filter}`
     const IncreaseQty = (product) => {
         const count = 1;
         setTimeout(() => {
@@ -56,7 +57,7 @@ const AddToCart = () => {
     }
     useEffect(() => {
         dispatch(fetchProduct(URI))
-    }, [dispatch, currentPage, keyword, filter])
+    }, [dispatch, currentPage, keyword])
     const addCart = (product) => {
         setTimeout(() => {
             dispatch(addToCart({ newItems: product, itemQuantity: 1, price: product.price }))
@@ -73,7 +74,9 @@ const AddToCart = () => {
         boxShadow: 24,
         p: 4,
     };
-
+    const filteredProducts = allProduct && allProduct.result && allProduct.result.filter(val => {
+        return (filter === '' && val) || (val.product && val.product.toLocaleLowerCase().includes(filter.toLowerCase()) && val)
+    })
 
     return (
         <section className={`relative`}>
@@ -100,73 +103,84 @@ const AddToCart = () => {
                     (
                         <div className='py-[3rem]'>
                             <div className="mx-auto container px-[2rem]">
-                                <div className={`grid lg:grid-cols-3 xl:grid-cols-4 md:grid-cols-2 grid-cols-1 justify-center gap-4`}>
-                                    {
-                                        allProduct && allProduct.result && allProduct.result.map(product => {
-                                            const cartItem = cartItems?.find((item) => item?._id === product._id);
-                                            const cartQty = cartItem ? cartItem?.quantity : 0;
-                                            return (
-                                                <div className='mb-[5rem] p-2 bg-[#ffff] shadow-lg' key={product._id}>
-                                                    <div className={`cursor-pointer`}>
-                                                        <div className={`flex flex-col justify-end h-[15rem] sm:h-[30rem] bg-[url('https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/96/7057222/1.jpg?9360')] bg-no-repeat bg-center  bg-contain`}>
+                                {
+                                    filteredProducts?.length ? (
+                                        <div className={`grid lg:grid-cols-3 xl:grid-cols-4 md:grid-cols-2 grid-cols-1 justify-center gap-4`}>
+                                            {
+                                                filteredProducts.map(product => {
+                                                    const cartItem = cartItems?.find((item) => item?._id === product._id);
+                                                    const cartQty = cartItem ? cartItem?.quantity : 0;
+                                                    return (
+                                                        <div className='mb-[5rem] p-2 bg-[#ffff] shadow-lg' key={product._id}>
+                                                            <div className={`cursor-pointer`}>
+                                                                <div className={`flex flex-col justify-end h-[15rem] sm:h-[30rem] bg-[url('https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/96/7057222/1.jpg?9360')] bg-no-repeat bg-center  bg-contain`}>
 
-                                                            <div className='text-white hidden md:block hover:bg-[rgb(205,204,197,0.5)] bg-[rgb(205,204,197)] py-3 text-center translate-y-[10px] transform hover:translate-y-0 duration-[500ms]' onClick={() => operate(product)}>
-                                                                Quick view
-                                                            </div>
-                                                        </div>
-
-                                                    </div>
-                                                    <div className='py-2'>
-                                                        <p className='text-[18px] font-[400] text-[#2f2e2e]'>{product.product}</p>
-                                                        <p className='text-[16px] font-[400] text-[#605e5e]'>${product.price}</p>
-                                                    </div>
-                                                    <input type="text" value='1' className={`hidden count`} />
-                                                    <div>
-                                                        {cartQty <= 0 ? (
-                                                            <div className='cursor-pointer transform duration-[500ms] text-center bg-[#44dbbd] hover:bg-[#13322c] text-white py-3' onClick={() => addCart(product)}>
-                                                                Add to cart
-                                                            </div>
-                                                        ) : (
-                                                            <div>
-                                                                {/* <div className="text-center">
-                                                                        <Spinner aria-label="Center-aligned spinner example" />
-                                                                    </div> */}
-                                                                <div className='flex text-center'>
-                                                                    <div className={`flex-[0.5] cursor-pointer transform duration-[500ms] text-center bg-[#44dbbd] hover:bg-[#13322c] text-white py-3  text-[1rem] rounded-md`}>
-
-                                                                        {
-                                                                            cartQty !== 1 ? (<span class="material-symbols-outlined " onClick={() => decreaseQty(product)}>
-                                                                                remove
-                                                                            </span>) : (
-                                                                                <span class="material-symbols-outlined cursor-pointer" onClick={() => deleteCart(product)}>
-                                                                                    remove
-                                                                                </span>
-                                                                            )
-                                                                        }
-                                                                    </div>
-                                                                    <div className='flex-[1] py-3 text-[1.3rem]'>
-                                                                        {cartQty}</div>
-                                                                    <div className={`cursor-pointer rounded-md flex-[0.5] transform duration-[500ms] text-center bg-[#44dbbd] hover:bg-[#13322c] text-white py-3`}>
-                                                                        <span class="material-symbols-outlined" onClick={() => IncreaseQty(product)}>
-                                                                            add
-                                                                        </span>
+                                                                    <div className='text-white hidden md:block hover:bg-[rgb(205,204,197,0.5)] bg-[rgb(205,204,197)] py-3 text-center translate-y-[10px] transform hover:translate-y-0 duration-[500ms]' onClick={() => operate(product)}>
+                                                                        Quick view
                                                                     </div>
                                                                 </div>
-                                                            </div>
 
-                                                        )}
-                                                    </div>
-                                                </div>
-                                            )
-                                        })
+                                                            </div>
+                                                            <div className='py-2'>
+                                                                <p className='text-[18px] font-[400] text-[#2f2e2e]'>{product.product}</p>
+                                                                <p className='text-[16px] font-[400] text-[#605e5e]'>${product.price}</p>
+                                                            </div>
+                                                            <input type="text" value='1' className={`hidden count`} />
+                                                            <div>
+                                                                {cartQty <= 0 ? (
+                                                                    <div className='cursor-pointer transform duration-[500ms] text-center bg-[#44dbbd] hover:bg-[#13322c] text-white py-3' onClick={() => addCart(product)}>
+                                                                        Add to cart
+                                                                    </div>
+                                                                ) : (
+                                                                    <div>
+                                                                        {/* <div className="text-center">
+                                                                                                        <Spinner aria-label="Center-aligned spinner example" />
+                                                                                                    </div> */}
+                                                                        <div className='flex text-center'>
+                                                                            <div className={`flex-[0.5] cursor-pointer transform duration-[500ms] text-center bg-[#44dbbd] hover:bg-[#13322c] text-white py-3  text-[1rem] rounded-md`}>
+
+                                                                                {
+                                                                                    cartQty !== 1 ? (<span class="material-symbols-outlined " onClick={() => decreaseQty(product)}>
+                                                                                        remove
+                                                                                    </span>) : (
+                                                                                        <span class="material-symbols-outlined cursor-pointer" onClick={() => deleteCart(product)}>
+                                                                                            remove
+                                                                                        </span>
+                                                                                    )
+                                                                                }
+                                                                            </div>
+                                                                            <div className='flex-[1] py-3 text-[1.3rem]'>
+                                                                                {cartQty}</div>
+                                                                            <div className={`cursor-pointer rounded-md flex-[0.5] transform duration-[500ms] text-center bg-[#44dbbd] hover:bg-[#13322c] text-white py-3`}>
+                                                                                <span class="material-symbols-outlined" onClick={() => IncreaseQty(product)}>
+                                                                                    add
+                                                                                </span>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+
+                                                                )}
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </div>
+                                    ) : (
+                                        <div className={`text-center text-[3rem] py-[3rem]`}>
+                                            <p>No products found for your search: "{filter}"</p>
+                                        </div>
+                                    )
+                                }
+
+                            </div>
+                            {filteredProducts?.length > 0 && (
+                                <div className="flex overflow-x-auto justify-center">
+                                    {
+                                        allProduct.totalPages && <Pagination currentPage={currentPage} totalPages={allProduct.totalPages} onPageChange={onPageChange} showIcons />
                                     }
                                 </div>
-                            </div>
-                            <div className="flex overflow-x-auto justify-center">
-                                {
-                                    allProduct.totalPages && <Pagination currentPage={currentPage} totalPages={allProduct.totalPages} onPageChange={onPageChange} showIcons />
-                                }
-                            </div>
+                            )}
                         </div>
                     )
                 )
