d7a5aaf9281b2ff57acd07d1b3426688a7d6b859
Abassgold <Abasskola10@gmail.com>
2024-10-09 01:07:38 +0100

    added logout section

--- src/component/Loader/Loader.jsx
+++ src/component/Loader/Loader.jsx
@@ -15,8 +15,8 @@ const Loader = ({ msg }) => {
 
                     <div class="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
                         <div class="relative flex justify-center items-center">
-                            <div className="absolute border-gray-300 md:h-[5rem] h-[4rem] w-[4rem] md:w-[5rem] animate-spin rounded-full border-[4px] border-y-[#16a34a]"></div>
-                            <img src="https://res.cloudinary.com/abasskola/image/upload/v1724107912/Preview_b8xkcn.jpg" className="rounded-full md:h-[5rem] md:w-[5rem] h-[4rem] w-[4rem]  p-[0.8rem]" />
+                            <div className="absolute border-gray-300 md:h-[4.3rem] md:w-[4.3rem] h-[3.2rem] w-[3.2rem] animate-spin rounded-full border-[4px] border-y-[#16a34a]"></div>
+                            <img src="https://res.cloudinary.com/abasskola/image/upload/v1724107912/Preview_b8xkcn.jpg" className="rounded-full md:h-[5rem] md:w-[5rem] h-[4rem] w-[4rem] p-[0.8rem] animate-scaleUpDown" />
                         </div>
                         <div className='text-[#ffff] mt-[0.1rem] italic'>
                             <h1>{msg}</h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
+  content: [
     './src/**/*.{js,jsx,ts,tsx}',
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
 
@@ -9,9 +9,18 @@ export default {
     extend: {
       screens: {
         'xs': '400px',
-      }
+      },
+      keyframes: {
+        scaleUpDown: {
+          '0%, 100%': { transform: 'scale(1)' }, // Original size
+          '50%': { transform: 'scale(1.1)' },   // Slightly bigger
+        },
+      },
+      animation: {
+        scaleUpDown: 'scaleUpDown 2s infinite', // 2s duration, infinite repeat
+      },
     },
-    
+
   },
   plugins: [
     function ({ addBase, theme }) {
