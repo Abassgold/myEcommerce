3604e0382e9c0ddfbade1f22aec1bf9189e37d1d
Abassgold <Abasskola10@gmail.com>
2024-10-10 10:30:02 +0100

    'jsjk

--- src/component/Navbar/ReactNavbar.jsx
+++ src/component/Navbar/ReactNavbar.jsx
@@ -30,7 +30,7 @@ const ReactNavbar = () => {
 
     }
     useEffect(() => {
-        setDropdown(!dropdown)
+        setDropdown(false)
     }, [location])
     return (
         <div className='bg-[#2f2e2e] py-1'>
@@ -87,7 +87,7 @@ const ReactNavbar = () => {
                                         <img className="h-8 w-8 rounded-full object-cover" src={user ? user?.photo?.url : 'https://cdn-icons-png.flaticon.com/512/149/149071.png'} alt="" />
                                     </button>
                                 </div>
-                                {!dropdown && (
+                                {dropdown && (
                                     <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                                         {user && (
                                             <div>
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: true,
-    port: process.env.PORT || 3000, 
-  },
   plugins: [react()],
   build: {
     rollupOptions: {
